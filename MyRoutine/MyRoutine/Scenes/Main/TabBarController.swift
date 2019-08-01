3c53450d10e20f36ded2d8a7dc110e98aaeac3de
Bùi Xuân Huy <buixuanhuy5798@gmail.com>
2019-08-01 15:45:43 +0700

    FixTabBar

--- MyRoutine/MyRoutine/Scenes/Main/TabBarController.swift
+++ MyRoutine/MyRoutine/Scenes/Main/TabBarController.swift
@@ -7,26 +7,10 @@
 //
 
 class TabBarController: UITabBarController {
-    // MARK: - Variables
-    let diaryVC = Storyboards.diary.instantiateViewController(withIdentifier: "Diary")
-    let statisticAll = Storyboards.statisticAll.instantiateViewController(withIdentifier: "StatisticAll")
-    let setting = Storyboards.setting.instantiateViewController(withIdentifier: "Setting")
+   
     // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTabBar()
-    }
-    // MARK: - Setup
-    func setupTabBar() {
-        diaryVC.tabBarItem = UITabBarItem(title: "Nhật ký",
-                                          image: UIImage(named: "iconDiary"), tag: 0)
-        statisticAll.tabBarItem = UITabBarItem(title: "Thống kê",
-                                               image: UIImage(named: "iconStatistic"), tag: 1)
-        setting.tabBarItem = UITabBarItem(title: "Cài đặt",
-                                          image: UIImage(named: "iconSetting"), tag: 2)
-        tabBar.tintColor = #colorLiteral(red: 1, green: 0.6677710414, blue: 0.7362043262, alpha: 1)
-        tabBar.unselectedItemTintColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        self.viewControllers = [diaryVC, statisticAll, setting]
     }
     
 }

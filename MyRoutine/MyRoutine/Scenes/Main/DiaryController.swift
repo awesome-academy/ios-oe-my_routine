5168d158b733f20268d215cd20901fdac242431f
Bùi Xuân Huy <buixuanhuy5798@gmail.com>
2019-08-01 09:45:14 +0700

    Xong Tab Bar

---
+++ MyRoutine/MyRoutine/Scenes/Main/DiaryController.swift
@@ -0,0 +1,15 @@
+//
+//  DiaryController.swift
+//  MyRoutine
+//
+//  Created by Bùi Xuân Huy on 8/1/19.
+//  Copyright © 2019 huy. All rights reserved.
+//
+
+class DiaryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ MyRoutine/MyRoutine/Scenes/Main/SettingController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingController.swift
+//  MyRoutine
+//
+//  Created by Bùi Xuân Huy on 8/1/19.
+//  Copyright © 2019 huy. All rights reserved.
+//
+
+class SettingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}

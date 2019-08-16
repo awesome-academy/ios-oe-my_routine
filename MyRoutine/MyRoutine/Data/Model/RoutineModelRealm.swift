bbca9bceaf14ce2502040b760843a7d3acc02d20
Bùi Xuân Huy <buixuanhuy5798@gmail.com>
2019-08-16 10:14:53 +0700

    Thay đổi model Realm

--- MyRoutine/MyRoutine/Data/Model/RoutineModelRealm.swift
+++ MyRoutine/MyRoutine/Data/Model/RoutineModelRealm.swift
@@ -5,43 +5,17 @@
 //  Created by Bùi Xuân Huy on 8/2/19.
 //  Copyright © 2019 huy. All rights reserved.
 //
-import RealmSwift
-
-class RealmString: Object {
-    @objc dynamic var stringValue = ""
-}
 
 class RoutineModelRealm: Object {
     
     // MARK: - Properties
-    @objc dynamic var idRoutine: String = ""
-    @objc dynamic var name: String = ""
-    @objc dynamic var dayStart: String = ""
-    @objc dynamic var period: String = ""
-    let repeatRou = List<RealmString>()
-    let remindRou = List<RealmString>()
-    var repeatRoutine: [String] {
-        get {
-            return repeatRou.map { $0.stringValue }
-        }
-        set {
-            repeatRou.removeAll()
-            repeatRou.append(objectsIn: newValue.map { RealmString(value: [$0]) })
-        }
-    }
-    var remind: [String] {
-        get {
-            return remindRou.map { $0.stringValue }
-        }
-        set {
-            remindRou.removeAll()
-            remindRou.append(objectsIn: newValue.map { RealmString(value: [$0]) })
-        }
-    }
-    
-    // MARK: Method
-    override static func ignoredProperties() -> [String] {
-        return ["remind", "repeatRoutine"]
-    }
+    @objc dynamic var idRoutine = 0
+    @objc dynamic var name = ""
+    @objc dynamic var dayStart = Date()
+    @objc dynamic var period = 4
+    @objc dynamic var targetRoutine: TargetModel?
+    @objc dynamic var repeatRoutine: RepeatModel?
+    let remindRoutine = List<RemindModel>()
+    @objc dynamic var doneCount = 0
     
 }
---
+++ MyRoutine/MyRoutine/Extension/List+.swift
@@ -0,0 +1,15 @@
+//
+//  List+.swift
+//  MyRoutine
+//
+//  Created by Bùi Xuân Huy on 8/12/19.
+//  Copyright © 2019 huy. All rights reserved.
+//
+
+extension List {
+    
+    func toArray<T>(type: T.Type) -> [T] {
+        return compactMap { $0 as? T }
+    }
+    
+}
